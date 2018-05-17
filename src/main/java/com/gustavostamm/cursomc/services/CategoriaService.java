763eef7a56a53cacd406b6c01fff73c00b24758d
gustavostamm <gustavo.stamm@gmail.com>
2018-05-17 16:53:40 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/gustavostamm/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.gustavostamm.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavostamm.cursomc.domain.Categoria;
+import com.gustavostamm.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	public CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);  
+		return obj.orElse(null); 
+	}
+
+}
