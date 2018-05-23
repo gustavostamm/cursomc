4de879114690574334d8d45befaf9f65e75b20b4
gustavostamm <gustavo.stamm@gmail.com>
2018-05-23 19:57:55 -0300

    endpoint/cliente/id disponivel

--- src/main/java/com/gustavostamm/cursomc/domain/Cliente.java
+++ src/main/java/com/gustavostamm/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.gustavostamm.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -29,6 +30,7 @@ public class Cliente implements Serializable{
 	private String cpfoucnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
